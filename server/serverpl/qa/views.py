3ef323508b835317d2aebd93c0680a886285900c
HARDY ELISE <ehardy@etud.u-pem.fr>
2018-06-15 10:22:35 +0200

    search fix

--- server/serverpl/qa/views.py
+++ server/serverpl/qa/views.py
@@ -170,10 +170,7 @@ class QuestionsSearchView(QuestionIndexView):
     Display a ListView page inherithed from the QuestionIndexView filtered by
     the search query and sorted by the different elements aggregated.
     """
-    model = Question
-    paginate_by = 10
-    context_object_name = 'questions'
-    template_name = 'qa/index.html'
+    
     
     def get_queryset(self):
         result = super(QuestionsSearchView, self).get_queryset()
